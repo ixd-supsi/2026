c3a759130d73c246b9910b4b4c367a5733f1a586
Andreas Gysin <andreas@ertdfgcvb.ch>
2026-03-05 11:23:38 +0100

    Update script.js

--- esempi/es04_moon_surface/script.js
+++ esempi/es04_moon_surface/script.js
@@ -1,27 +1,44 @@
+
+let piastrella
+
+function preload() {
+	piastrella = loadImage("tiles/moon_tiny.png")
+}
+
 function setup() {
 	createCanvas(400, 400, WEBGL)
 
 }
 
 function draw() {
-	background(220, 0, 0)
+	background(0)
+	stroke(255)
+
+	rotateX(PI/3)
+
+	if (mouseIsPressed) {
+		rotateZ(mouseX * 0.01)
+	}
+
 
 	const spaziatura = 18
 
-	const numPuntiX = 19
-	const numPuntiY = 19
+	const numPuntiX = piastrella.width
+	const numPuntiY = piastrella.height
 
 	const margineX = - (numPuntiX - 1) * spaziatura / 2
 	const margineY = - (numPuntiY - 1) * spaziatura / 2
 
-	strokeWeight(8)
 
+	noFill()
+	beginShape(POINTS)
 	for (let j=0; j<numPuntiY; j++) {
 		for(let i=0; i<numPuntiX; i++) {
 			const px = i * spaziatura + margineX
 			const py = j * spaziatura + margineY
-			point(px, py)
+			vertex(px, py)
 		}
 	}
+	endShape()
 }
 
